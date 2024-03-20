62c02aecd61f78ced36bf36c01ccbdf432f3ae19
NikolayShavar <fargon45821@gmail.com>
2024-03-20 20:27:19 +0200

    initialSlide: 1,

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,30 @@
-$('.center').slick({
-    centerMode: true,
-    centerPadding: '120px',
-    rows: 2,
-    slidesToShow: 2,
-    sliderToScroll:1,
-    arrows: false,
-    infinite: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2
-        }
+$(".center").slick({
+  centerMode: true,
+  centerPadding: "120px",
+  rows: 2,
+  initialSlide: 1,
+  slidesToShow: 2,
+  sliderToScroll: 1,
+  arrows: false,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
