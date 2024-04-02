5081f38c9a2e3f28b15a7941003b6a519e672971
NikolayShavar <fargon45821@gmail.com>
2024-04-02 16:09:11 +0300

    last slider 4 end

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,48 @@ $(".center").slick({
   ],
 });
 
+$(".center-row").slick({
+  centerMode: true,
+  centerPadding: "330px",
+  initialSlide: 1,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: $(".choose-combo-arrows__left"),
+  nextArrow: $(".choose-combo-arrows__right"),
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1680,
+      settings: {
+        slidesToShow: 2,
+        centerPadding: "250px",
+      },
+    },
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 2,
+        centerPadding: "100px",
+      },
+    },
+    {
+      breakpoint: 760,
+      settings: {
+        slidesToShow: 1,
+        centerPadding: "50px",
+      },
+    },
+
+    
+  ],
+});
+
 $(".center").on("afterChange", function (event, slick, currentSlide) {
   $(".choose-arrows__left").toggle(currentSlide !== 0);
   $(".choose-arrows__right").toggle(currentSlide !== slick.slideCount - 1);
+});
+
+$(".center-row").on("afterChange", function (event, slick, currentSlide) {
+  $(".choose-combo-arrows__left").toggle(currentSlide !== 0);
+  $(".choose-combo-arrows__right").toggle(currentSlide !== slick.slideCount - 1);
 });
